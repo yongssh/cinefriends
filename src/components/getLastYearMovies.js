7a1381377517b8b247b82fe25cc085bee2dffa52
Yong-Yu Huang <115194864+yongssh@users.noreply.github.com>
2024-10-27 22:21:35 -0500

    Update getLastYearMovies.js

--- src/components/getLastYearMovies.js
+++ src/components/getLastYearMovies.js
@@ -1,19 +1,17 @@
-// getLastYearMovies.js
-
 export const getLastYearMovies = (diaryData) => {
-  // Ensure diaryData is an array and not empty
+  // make sure diaryData is an array and not empty
   if (!Array.isArray(diaryData)) {
       console.error('diaryData is not an array:', diaryData);
-      return []; // Return empty array or handle error appropriately
+      return []; 
   }
   
   const currentYear = new Date().getFullYear();
   const lastYear = currentYear - 1;
   console.log('Filtering movies for the year:', lastYear);
 
-  // Filter movies for last year
+  // filter movies for last calendar year
   const filteredMovies = diaryData.filter(movie => {
-      // Ensure movie has a valid date_watched property
+      // ensure movie has a valid date_watched 
       if (!movie.date_watched) {
           console.error('Invalid movie data - missing date_watched:', movie);
           return false;
@@ -27,14 +25,14 @@ export const getLastYearMovies = (diaryData) => {
 
   console.log('Filtered Movies:', filteredMovies);
 
-  // Sort filtered movies by date_watched in descending order
+  // sort filtered movies by date_watched in descending order
   const sortedMovies = filteredMovies.sort((a, b) => new Date(b.date_watched) - new Date(a.date_watched));
 
-  // Convert rewatch from string to boolean where applicable
+  // convert rewatch from string to boolean 
   const convertedmovies = sortedMovies.map(movie => ({
       ...movie,
-      rewatch: movie.rewatch === 'true', // Assuming rewatch is a string 'true' or 'false'
-      rating: parseInt(movie.rating) || 'No Rating' // Convert rating to integer, handle NaN with fallback
+      rewatch: movie.rewatch === 'true', 
+      rating: parseInt(movie.rating) || 'No Rating' 
   }));
 
   console.log('Movies with Rewatch and Rating:', convertedmovies);
